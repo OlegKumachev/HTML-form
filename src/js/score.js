6eeadcdf3b79fa0a4f65c72acfce6f525270ed6c
OlegKumachev <117755901+OlegKumachev@users.noreply.github.com>
2024-02-13 20:33:59 +0300

    Update score.js

--- src/js/score.js
+++ src/js/score.js
@@ -1,19 +1,1 @@
-// export default class Score {
-//   constructor(id) {
-//     this.element = document.getElementById(id);
-//   }
 
-//   update(score) {
-//     this.element.textContent = `Score: ${score}`;
-//   }
-// }
-
-export default class Score {
-  constructor(id) {
-    this.element = document.getElementById(id);
-  }
-
-  update(score) {
-    this.element.textContent = `Score: ${score}`;
-  }
-}
