5b5f767dcd8a48c216fa017ea1fa571833cfb530
HelegKumachev <kumachev.oleg@gmail.com>
2024-02-29 22:37:33 +0300

    appveyor commit

--- src/js/goblin.js
+++ src/js/goblin.js
@@ -2,8 +2,8 @@ export default function playGoblin(game) {
   document.addEventListener("DOMContentLoaded", () => {
     let score = 0;
     const scoreDisplay = document.getElementById('score-display');
-    let currentGoblinPosition = -1; // Изначально гоблин не заспавнен
-    let clickedOnGoblin = false; // Флаг, отслеживающий клик по гоблину
+    let currentGoblinPosition = -1;
+    let clickedOnGoblin = false;
 
     function moviGoblin(game) {
       const img = document.createElement("img");
@@ -14,25 +14,24 @@ export default function playGoblin(game) {
       const field = document.querySelectorAll(".cellField");
       const newPosition = Math.floor(Math.random() * field.length);
 
-      // Если гоблин уже существует и переместился в другую ячейку, засчитываем промах
       if (currentGoblinPosition !== -1 && newPosition !== currentGoblinPosition && !clickedOnGoblin) {
         game.missedGoblinHits++;
         game.updateMissedHits();
       }
 
-      // Обновляем позицию гоблина
+
       currentGoblinPosition = newPosition;
-      clickedOnGoblin = false; // Сбрасываем флаг после каждого перемещения гоблина
+      clickedOnGoblin = false; 
 
       field.forEach(cell => cell.innerHTML = ''); 
       field[newPosition].appendChild(img);
 
-      // Добавляем обработчик клика по гоблину
+
       img.addEventListener('click', () => {
         score++;
         scoreDisplay.textContent = `Score: ${score}`;
-        clickedOnGoblin = true; // Устанавливаем флаг в true при клике по гоблину
-        field[newPosition].removeChild(img); // Удаляем гоблина из ячейки
+        clickedOnGoblin = true; 
+        field[newPosition].removeChild(img); 
       });
     }
 
